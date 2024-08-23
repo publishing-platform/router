62291597f6c04740dd1d96fe72dbcd891291a6e2
neilha <neilharrold@gmail.com>
2024-08-23 20:52:39 +0100

    adds reload route to api handler

--- internal/router/router-api.go
+++ internal/router/router-api.go
@@ -8,6 +8,27 @@ import (
 func NewAPIHandler(rout *Router) (api http.Handler, err error) {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/reload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.Header().Set("Allow", http.MethodPost)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		// Send a message to the Router goroutine which will start a reload if necessary.
+		// If the channel is already full, no message will be sent and the request
+		// won't be blocked.
+		select {
+		case rout.ReloadChan <- true:
+		default:
+		}
+		log.Println("router: reload queued")
+		w.WriteHeader(http.StatusAccepted)
+		_, err := w.Write([]byte("Reload queued"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
 	mux.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			w.Header().Set("Allow", http.MethodGet)
